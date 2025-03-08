2e1d74e8a57249f16a6e414d3cdbba98f9dec593
parth bhutada <parthbhutada13@gmail.com>
2025-03-08 23:38:59 +0530

    Implementing methods

--- Rectangle.java
+++ Rectangle.java
@@ -3,3 +3,12 @@ class Rectangle extends Shape {
 	public Rectangle(double length, double width) {
 		super(length, width, 0, 4);
 	}
+
+	public double calculateArea() {
+		return dim1 * dim2;
+	}
+
+	public double calculatePerimeter() {
+		return 2 * (dim1 + dim2);
+	}
+}
