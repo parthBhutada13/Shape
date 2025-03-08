a4588f8cc0b6697a2c201993f445dd3f0bf5b1e7
parth bhutada <parthbhutada13@gmail.com>
2025-03-08 23:45:25 +0530

    Implementing methods

--- Cuboid.java
+++ Cuboid.java
@@ -3,3 +3,17 @@ class Cuboid extends Shape {
 	public Cuboid(double length, double breadth, double height) {
 		super(length, breadth, height, 12); // A cuboid has 12 edges
 	}
+
+	public double calculateArea() {
+		return 2 * (dim1 * dim2 + dim2 * dim3 + dim3 * dim1); // Surface area of a cuboid
+	}
+
+	public double calculatePerimeter() {
+		return 4 * (dim1 + dim2 + dim3); // Sum of all edges
+	}
+
+	@Override
+	public double calculateVolume() {
+		return dim1 * dim2 * dim3; // Volume of a cuboid
+	}
+}
