905eb3fac3d9261e2b9235b43f65531c1507fa99
parth bhutada <parthbhutada13@gmail.com>
2025-03-08 23:40:23 +0530

    Implementing methods

--- Circle.java
+++ Circle.java
@@ -3,3 +3,12 @@ class Circle extends Shape {
 	public Circle(double radius) {
 		super(radius, 0, 0, 0); // A circle has no sides
 	}
+
+	public double calculateArea() {
+		return Math.PI * dim1 * dim1;
+	}
+
+	public double calculatePerimeter() {
+		return 2 * Math.PI * dim1;
+	}
+}
