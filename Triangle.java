9326c9758a743c30928bd8decde0c0dd0def301a
parth bhutada <parthbhutada13@gmail.com>
2025-03-08 23:42:49 +0530

    im

--- Triangle.java
+++ Triangle.java
@@ -3,3 +3,13 @@ class Triangle extends Shape {
 	public Triangle(double a, double b, double c) {
 		super(a, b, c, 3);
 	}
+
+	public double calculateArea() {
+		double s = (dim1 + dim2 + dim3) / 2;
+		return Math.sqrt(s * (s - dim1) * (s - dim2) * (s - dim3));
+	}
+
+	public double calculatePerimeter() {
+		return dim1 + dim2 + dim3;
+	}
+}
