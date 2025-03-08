e9344af4ad71083e5dad2591cee306cd4da7ee87
parth bhutada <parthbhutada13@gmail.com>
2025-03-08 23:37:13 +0530

    Defining Constructor and Methods

--- Shape.java
+++ Shape.java
@@ -4,4 +4,21 @@ abstract class Shape {
 	protected double dim3;
 	protected int num_sides;
 
+	// Constructor with 0 parameters
+	public Shape() {
+		dim1 = dim2 = dim3 = 0;
+		num_sides = 0;
+	}
+
+	// Parameterized constructor
+	public Shape(double dim1, double dim2, double dim3, int num_sides) {
+		this.dim1 = dim1;
+		this.dim2 = dim2;
+		this.dim3 = dim3;
+		this.num_sides = num_sides;
+	}
+
+	abstract public double calculateArea();
+	abstract public double calculatePerimeter();
+
 	
