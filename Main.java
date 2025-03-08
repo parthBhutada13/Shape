21fb8e96916da593400430db41a506b6d39ef205
parth bhutada <parthbhutada13@gmail.com>
2025-03-08 23:47:48 +0530

    Rectangle Methods

--- Main.java
+++ Main.java
@@ -5,4 +5,13 @@ class Main {
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
 
+		System.out.println("Enter length:- ");
+        double length = Double.parseDouble(scan.nextLine());
+
+        System.out.println("Enter width:- ");
+        double width = Double.parseDouble(scan.nextLine());
+
+        Shape rectangle = new Rectangle(length, width);
+        printShapeDetails(rectangle, "Rectangle");
+
         
