b7710098680cae49517e654d0a1ebc7116147671
parth bhutada <parthbhutada13@gmail.com>
2025-03-08 23:41:33 +0530

    Implementing methods

--- Square.java
+++ Square.java
@@ -3,3 +3,12 @@ class Square extends Shape {
 	public Square(double side) {
 		super(side, 0, 0, 4);
 	}
+
+	public double calculateArea() {
+		return dim1 * dim1;
+	}
+
+	public double calculatePerimeter() {
+		return 4 * dim1;
+	}
+}
