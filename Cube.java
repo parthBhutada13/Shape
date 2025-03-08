0a1380d78a700de74d6207c21828cfd0d8a428a8
parth bhutada <parthbhutada13@gmail.com>
2025-03-08 23:44:26 +0530

    Implementing methods

--- Cube.java
+++ Cube.java
@@ -3,3 +3,17 @@ class Cube extends Shape {
 	public Cube(double side) {
 		super(side, side, side, 12); // A cube has 12 edges
   }
+
+	public double calculateArea() {
+		return 6 * dim1 * dim1; // Surface area of a cube
+	}
+
+	public double calculatePerimeter() {
+		return 12 * dim1; // Sum of all edges
+	}
+
+	@Override
+	public double calculateVolume() {
+		return dim1 * dim1 * dim1; // Volume of a cube
+	}
+}
